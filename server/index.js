74087d2cdfafa12a967d8b154f1d9a2a35b56774
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2023-11-14 16:37:36 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-app.use(cors());
+app.use(cors(
+          {
+        origin: ["https://orsod.vercel.app"],
+        methods: ["POST","GET"],
+        credentials: true
+  }
+));
 app.use(express.json({limit: '200mb', extended: true}))
 const _Port = "3001"
 
