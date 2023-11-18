2a6a5693e69d2c7f1a96a996ff1adc7c22b73cb2
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2023-11-18 16:26:21 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ const mongoose = require("mongoose")
 mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.oiijcet.mongodb.net/${database}?retryWrites=true&w=majority`,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000
 }
       ).catch(err => console.log(err.reason)); 
 
