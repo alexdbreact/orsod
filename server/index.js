6188bc74687c77260b64212d2862a1e7ca8e7fb9
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2023-11-19 15:33:48 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const cors = require("cors")
 app.use(cors(
 
 ));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://orsod.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.use(express.json({limit: '200mb', extended: true}))
 app.use(express.urlencoded({limit: '200mb', extended: true}))
@@ -40,9 +46,7 @@ app.post("/createUser", async (req, res)=>{
 
     const user = req.body;
     const newUser = new UserModel(req.body);
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    
     await newUser.save();
     res.json(req.body)
 })
