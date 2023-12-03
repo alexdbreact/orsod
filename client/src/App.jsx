72c095a5ce7a2e25f306f05b1204e40c596cd505
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2023-12-03 16:53:45 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -127,7 +127,7 @@ function App() {
                 onChange={(e) => {
                   setPlace(e.target.value);
                 }}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center">
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 font-bold dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center">
                 <option className="bg-gray-500">  اختار الحي الواقع به المخالفة *</option>
                 <option>شرق</option>
                 <option>وسط</option>
@@ -147,7 +147,7 @@ function App() {
                 onChange={(e) => {
                   setSort(e.target.value);
                 }}
-                className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center">
+                className="bg-gray-50 border font-bold  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center">
                 <option > اختار نوع المخالفة *   </option>
                 <option>نظافة / تراكم قمامة </option>
                 <option >هدم / ترميم / بناء مخالف</option>
