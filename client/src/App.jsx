c1beadf7ba6f023be11e2959e65a9a6869ea9ac8
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2023-12-12 21:33:53 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
             .then(setTimeout(function(){
               location.reload();
                   alert("تم ارسال المخالفة بنجاح .. شكراً لسيادتكم");
-          }, 5000))   
+          }, 7000))   
                            
         } else {
           alert("لم يتم الارسال .. برجاء التأكد من ادخالك البيانات المطلوبة و فتح خدمات الموقع )(Location")
